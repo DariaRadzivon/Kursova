409007cfaee9d55b5fbac688289ee21f0151c3f2
SSviatoslaWW <sviatoslaw7779@gmail.com>
2025-12-11 08:58:41 +0200

    Initial Commit

---
+++ LunaReads/LunaReadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunaReadsApp.swift
+//  LunaReads
+//
+//  Created by Svituk on 11.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct LunaReadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
